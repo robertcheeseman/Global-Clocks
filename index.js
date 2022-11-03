91f2af1c7032990ee6ff9b1b1c276fb15f5f7263
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2022-11-02 21:29:15 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const secondHand = document.querySelector('.second-hand')
+const minHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
+
+const setDate = () => {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    if (seconds === 0) {
+        secondHand.style.transform = `rotate(90deg)`;
+        secondHand.style.transition = `none`;
+    } else {
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+        secondHand.style.transition = `all 0.5s`;
+    }
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    
+    console.log(seconds, mins, hours)
+}
+
+setInterval(setDate, 1000)
+
+setDate();
